3a71d6e5cee1636c07aceabd203079694cf0b455
Rich Reneau <rich.reneau@gmail.com>
2018-04-16 21:24:46 -0400

    modified for orientation tests

--- app/index.js
+++ app/index.js
@@ -1,18 +1,32 @@
 /*
- *  Program assists with analyzing gryoscope and accelerometer readings
+ * Entry point for the watch app
  */
-
+// import document from "document";
 let document = require("document");
 import { Gyroscope } from "gyroscope";
+import { OrientationSensor } from "orientation";
 
 let btnStop = document.getElementById("btn-stop");
 
 let demotext = document.getElementById("demotext");
 demotext.text = "Fitbit Studio rocks!";
 
-var gyro = new Gyroscope({ frequency: 1 });
+let orientation = new OrientationSensor({ frequency: 1 });
 
-gyro.onreading = function() {
+orientation.onreading = function() {
+  console.log("Orientation Sensor Reading: " +
+              "timestamp: " + orientation.timestamp,
+              "quaternion[0]: " + orientation.quaternion[0],
+              "quaternion[1]: " + orientation.quaternion[1],
+              "quaternion[2]: " + orientation.quaternion[2],
+              "quaternion[3]: " + orientation.quaternion[3]);
+}
+
+orientation.start();
+
+// var gyro = new Gyroscope({ frequency: 1 });
+
+// gyro.onreading = function() {
 
   // Peek the current sensor values
  // console.log("ts:", gyro.timestamp,
@@ -20,18 +34,19 @@ gyro.onreading = function() {
   //            "y:", gyro.y,
   //            "z:", gyro.z);
   
-  console.log("x:", gyro.x,
-              "y:", gyro.y,
-              "z:", gyro.z);
+ //  console.log("x:", gyro.x,
+              // "y:", gyro.y,
+              // "z:", gyro.z);
 
   // Stop monitoring the sensor
   // gyro.stop();
-}
+// }
 
 // Begin monitoring the sensor
-gyro.start();
+// gyro.start();
 
 btnStop.onactivate = function (evt) {
-  gyro.stop();
+  // gyro.stop();
+  orientation.stop();
 }
 
